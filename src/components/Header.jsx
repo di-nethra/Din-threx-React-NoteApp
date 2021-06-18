d536141e481302a29d28b2d94c794453808330b5
di-nethra <anjana.dinethra@gmail.com>
2021-06-18 22:42:35 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+function Header(){
+
+    return(
+        <header>
+        <h1>Din-threx Notes</h1>
+        </header>
+    )
+
+
+}
+
+
+export default Header;
---
+++ src/math.js
@@ -0,0 +1,24 @@
+
+var firstName="Anjana";
+
+
+function secondName(){
+    
+    var middlename="dinethra";
+    var name=firstName+" "+middlename;
+
+    return name;
+    
+    }
+
+    function lastName(){
+    
+        var lastName="Smarakoon";
+        var name=firstName+" "+lastName;
+        return name;
+
+        }
+
+
+        export default firstName;
+        export {secondName,lastName};
